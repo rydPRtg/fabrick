38fc10554865a4b020e13fb260accc56c70f9cb6
RydTGbot <rydpr24@gmail.com>
2025-01-02 14:50:44 +0200

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,8 @@ function getQueryParams() {
     const params = new URLSearchParams(window.location.search);
     return {
         username: params.get('username'),
-        balance: params.get('balance')
+        balance: params.get('balance'),
+        telegram_id: params.get('telegram_id')
     };
 }
 
@@ -19,17 +20,17 @@ displayUserInfo(queryParams.username, queryParams.balance);
 
 // Реализация кнопок выхода
 document.getElementById("exitButton1Container").addEventListener("click", () => {
-    window.location.href = `slot2/index.html?username=${queryParams.username}&balance=${queryParams.balance}`;
+    window.location.href = `slot2/index.html?username=${queryParams.username}&balance=${queryParams.balance}&telegram_id=${queryParams.telegram_id}`;
 });
 
 document.getElementById("exitButton2Container").addEventListener("click", () => {
-    window.location.href = `ruletka/index.html?username=${queryParams.username}&balance=${queryParams.balance}`;
+    window.location.href = `ruletka/index.html?username=${queryParams.username}&balance=${queryParams.balance}&telegram_id=${queryParams.telegram_id}`;
 });
 
 document.getElementById("exitButton3Container").addEventListener("click", () => {
-    window.location.href = `aviator/index.html?username=${queryParams.username}&balance=${queryParams.balance}`;
+    window.location.href = `aviator/index.html?username=${queryParams.username}&balance=${queryParams.balance}&telegram_id=${queryParams.telegram_id}`;
 });
 
 document.getElementById("exitButton4").addEventListener("click", () => {
-    window.location.href = `slot/slot.html?username=${queryParams.username}&balance=${queryParams.balance}`;
+    window.location.href = `slot/slot.html?username=${queryParams.username}&balance=${queryParams.balance}&telegram_id=${queryParams.telegram_id}`;
 });
